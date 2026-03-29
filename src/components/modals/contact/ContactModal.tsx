60393486dff6941c3fbdf6b3d1d5678050606dde
Devhubfusionx <jeeyalex7@gmail.com>
2026-03-29 13:02:48 +0100

    ui modal update

--- src/components/modals/contact/ContactModal.tsx
+++ src/components/modals/contact/ContactModal.tsx
@@ -22,12 +22,12 @@ export default function ContactModal() {
 
             <ModalContainer 
                 type="registration"
-                className="px-4 pb-4 pt-12 md:p-5 w-full md:w-[777px] min-h-[400px] md:h-[444px] md:min-h-[444px] flex flex-col justify-center"
+                className="p-4 md:p-5 w-full md:w-[777px] min-h-[400px] md:h-[444px] md:min-h-[444px] flex flex-col justify-center"
                 maxWidth="max-w-[420px] md:max-w-[777px]"
             >
                 <ModalCloseButton 
                     onClick={closeModal} 
-                    className="flex h-9 w-9 items-center justify-center rounded-full border border-white/20 bg-black/30 hover:border-white/40 backdrop-blur-sm top-1.5 right-1.5 md:top-5 md:right-5 z-50 !absolute"
+                    className="flex h-9 w-9 items-center justify-center rounded-full border border-white/20 bg-black/30 hover:border-white/40 backdrop-blur-sm top-4 right-4 md:top-5 md:right-5 z-50"
                 />
 
                 <div className="flex flex-col md:flex-row w-full h-auto md:h-[calc(100%-24px)] gap-6 md:gap-6 items-center justify-between overflow-y-auto no-scrollbar [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] md:overflow-visible pb-4 md:pb-0">
