87c146f097263d376687d88eea04e9c0aa04f031
Devhubfusionx <jeeyalex7@gmail.com>
2026-04-10 11:43:29 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,20 +17,24 @@ export const metadata: Metadata = {
   openGraph: {
     title: "FindMe",
     description: "FindMe - Limitless Possibilities",
+    url: "https://findmeonline.com.ng",
+    siteName: "FindMe",
     images: [
       {
-        url: "/icons/Seo.svg",
+        url: "/icons/Seo.png",
         width: 1200,
         height: 630,
         alt: "FindMe SEO Image",
       },
     ],
+    locale: "en_US",
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "FindMe",
     description: "FindMe - Limitless Possibilities",
-    images: ["/icons/Seo.svg"],
+    images: ["/icons/Seo.png"],
   },
 };
 
